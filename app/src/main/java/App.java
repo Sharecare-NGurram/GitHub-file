5dc24832fab369d78146ec70ea9c6c4acc2dcb4e
Sharecare-NGurram <124046895+Sharecare-NGurram@users.noreply.github.com>
2023-02-24 17:09:43 +0530

    Update App.java

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class App {
+package demo;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+//    echo "# github-actions-example" >> README.md
+//    git init
+//    git add README.md
+//    git commit -m "first commit"
+//    git branch -M main
+//    git remote add origin https://github.com/Sharecare-NGurram/github-actions-example.git
+//    git push -u origin main
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
 }
