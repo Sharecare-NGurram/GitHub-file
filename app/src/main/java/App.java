7569f645696d3bc45d31fcd26cf723fd961430f0
Sharecare-NGurram <naveenreddy.gurram@genzeon.com>
2023-03-01 15:55:11 +0530

    updated

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -4,14 +4,6 @@ public class App {
     public String getGreeting() {
         return "Hello World!";
     }
-
-    echo "# github-actions-example" >> README.md
-    git init
-    git add README.md
-    git commit -m "first commit"
-    git branch -M main
-    git remote add origin https://github.com/Sharecare-NGurram/github-actions-example.git
-    git push -u origin main
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
     }
